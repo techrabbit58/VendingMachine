52e7f78de43a9a453d6289d4d9510dcd9e829fed
techrabbit58 <dzb@pruefbit.de>
2026-02-11 18:47:12 +0100

    refactor: __main__.py: remove unnecessary instance vars

--- src/vendingmachine/__main__.py
+++ src/vendingmachine/__main__.py
@@ -9,6 +9,11 @@ from .lib import get_product_by_button
 from .vendingmachine import VendingMachine, restock_products, refill_money_box
 
 
+def columnconfigure(frame: LabelFrame, *, columns: int, weight: int = 1) -> None:
+    for n in range(columns):
+        frame.columnconfigure(n, weight=weight)
+
+
 class App(tk.Tk):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
@@ -26,79 +31,65 @@ class App(tk.Tk):
 
         self.job_id = Queue()
 
-        self.button_area = LabelFrame(self, text="Products", borderwidth=5, labelanchor="n")
-        self.button_area.grid(column=0, row=0, padx=(15, 5), pady=10, ipady=5, sticky="nwse")
-        self.button_area.columnconfigure(0, weight=1)
-        self.button_area.columnconfigure(1, weight=1)
-        self.button_area.columnconfigure(2, weight=1)
+        button_area = LabelFrame(self, text="Products", borderwidth=5, labelanchor="n")
+        button_area.grid(column=0, row=0, padx=(15, 5), pady=10, ipady=5, sticky="nwse")
+        columnconfigure(button_area, columns=3)
 
-        self.buttons = []
         for button in BUTTONS:
             product = get_product_by_button(button)
             b = Button(
-                self.button_area,
+                button_area,
                 text=product.title(),
                 command=partial(self.on_select_product, button)
             )
             b.grid(sticky="n", column=1)
-            self.buttons.append(b)
 
-        self.coin_area = LabelFrame(self, text="Coins", borderwidth=5, labelanchor="n")
-        self.coin_area.grid(column=1, row=0, padx=(5, 15), pady=10, ipady=5, sticky="nwse")
-        self.coin_area.columnconfigure(0, weight=1)
-        self.coin_area.columnconfigure(1, weight=1)
-        self.coin_area.columnconfigure(2, weight=1)
+        coin_area = LabelFrame(self, text="Coins", borderwidth=5, labelanchor="n")
+        coin_area.grid(column=1, row=0, padx=(5, 15), pady=10, ipady=5, sticky="nwse")
+        columnconfigure(coin_area, columns=3)
 
-        self.coins = []
         for coin in COINS:
             c = Button(
-                self.coin_area,
+                coin_area,
                 text=coin.title(),
                 command=partial(self.on_insert_coin, coin)
             )
             c.grid(sticky="n", column=1)
-            self.coins.append(c)
 
-        self.display_area = LabelFrame(self, text="Display", borderwidth=5, labelanchor="n")
-        self.display_area.grid(
+        display_area = LabelFrame(self, text="Display", borderwidth=5, labelanchor="n")
+        display_area.grid(
             column=0, row=1, columnspan=2, padx=15, pady=(0, 5), ipady=5, sticky="we")
-        self.display_area.columnconfigure(0, weight=1)
-        self.display_area.columnconfigure(1, weight=1)
-        self.display_area.columnconfigure(2, weight=1)
+        columnconfigure(display_area, columns=3)
 
-        self.display = Label(self.display_area, text=self.vending_machine.check_display())
+        self.display = Label(display_area, text=self.vending_machine.check_display())
         self.display.grid(sticky="n", columnspan=3, column=0)
 
-        self.coin_return_area = LabelFrame(self, text="Coin Return", borderwidth=5, labelanchor="n")
-        self.coin_return_area.grid(
+        coin_return_area = LabelFrame(self, text="Coin Return", borderwidth=5, labelanchor="n")
+        coin_return_area.grid(
             column=0, row=2, columnspan=2, padx=15, pady=(0, 5), ipady=5, sticky="we")
-        self.coin_return_area.columnconfigure(0, weight=1)
-        self.coin_return_area.columnconfigure(1, weight=1)
-        self.coin_return_area.columnconfigure(2, weight=1)
+        columnconfigure(coin_return_area, columns=3)
 
         self.coin_return_button = Button(
-            self.coin_return_area,
+            coin_return_area,
             text="Return coins",
             command=self.on_return_coins
         )
         self.coin_return_button.grid(sticky="w", row=0, column=0)
 
         self.takeout_coins_button = Button(
-            self.coin_return_area,
+            coin_return_area,
             text="Take out",
             command=self.on_takeout_coins
         )
         self.takeout_coins_button.grid(sticky="w", row=1, column=0)
 
-        self.coin_return = Label(self.coin_return_area, text="Empty")
+        self.coin_return = Label(coin_return_area, text="Empty")
         self.coin_return.grid(sticky="w", row=0, column=1, columnspan=2)
 
         self.hopper_area = LabelFrame(self, text="Hopper", borderwidth=5, labelanchor="n")
         self.hopper_area.grid(
             column=0, row=3, columnspan=2, padx=15, pady=(0, 25), ipady=5, sticky="we")
-        self.hopper_area.columnconfigure(0, weight=1)
-        self.hopper_area.columnconfigure(1, weight=1)
-        self.hopper_area.columnconfigure(2, weight=1)
+        columnconfigure(self.hopper_area, columns=3)
 
         self.products = Label(self.hopper_area, text="Empty")
         self.products.grid(sticky="w", row=0, column=1, columnspan=2)
